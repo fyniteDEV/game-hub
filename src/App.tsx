fd28402a129842d28b8df5e8986d5944a0f2760b
fyniteDEV <jeneipeter001@gmail.com>
2025-04-13 22:18:40 +0200

    switched to material ui, a navbar built

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import { Grid, Button, Box } from "@mui/material";
 import NavBar from "./components/ui/NavBar";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`, // screens larger than 1024px
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <GridItem area="aside" bg="gold" display={{ base: "none", lg: "block" }}>
-        Aside
-      </GridItem>
-      <GridItem area="main" bg="dodgerblue">
-        Main
-      </GridItem>
-    </Grid>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main>
+        <Grid container>
+          <Grid size={12}>
+            <Box>
+              <NavBar />
+            </Box>
+          </Grid>
+
+          <Grid
+            size={{ lg: 6 }}
+            display={{
+              lg: "block",
+              xs: "none",
+            }}
+          >
+            <Box
+              sx={{
+                bgcolor: "gold",
+                p: 2,
+              }}
+            >
+              Aside
+            </Box>
+          </Grid>
+
+          <Grid size={{ lg: 6, xs: 12 }}>
+            <Box sx={{ bgcolor: "lightblue", p: 2 }}>Main</Box>
+          </Grid>
+        </Grid>
+      </main>
+    </ThemeProvider>
   );
 }
 
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -1,13 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { Box, Stack } from "@mui/material";
 import logo from "@/assets/logo.webp";
 
 const NavBar = () => {
   return (
-    <HStack>
-      <Image src={logo} boxSize={"60px"} />
-      <Text>NavBar</Text>
-    </HStack>
+    <Stack spacing={2} direction={"row"} alignItems={"center"}>
+      <Box component="img" sx={{ width: 70 }} src={logo}></Box>
+      <span>NavBar</span>
+    </Stack>
   );
 };
 
